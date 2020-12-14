3de93548a390dc7f1a5a93fefdc753e337ec4040
Philip Waldman <philip.waldman01@gmail.com>
2020-12-14 12:26:10 +0100

    Added training method to train to a certain accuracy

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ class Population:
         self.images = [Image(shape) for _ in range(pop_size)]
         self.fitness_sum = 0
 
-    def natural_selection(self):
+    def natural_selection(self, mutation_rate):
         self.calculate_fitnesses()
         new_images = [self.best_image()]
         for i in range(len(self.images) - 1):
             new_images.append(self.select_parent())
-            new_images[-1].mutate(0.05)
+            new_images[-1].mutate(mutation_rate)
         self.images = new_images.copy()
 
     def best_image(self):
@@ -69,19 +69,36 @@ class Population:
                 return image.copy()
 
 
-def train(generations):
+def train_gens(generations, mutation_rate):
     prev_best = pop.images[0]
     for gen in range(generations):
         print(f'Training generation {gen}...')
-        pop.natural_selection()
-        print(f'Best fitness: {pop.images[0].fitness}')
+        pop.natural_selection(mutation_rate)
         if False in (prev_best.image == pop.images[0].image):
             prev_best = pop.images[0]
             pop.images[0].save_image(os.path.join('output_data', f'gen {gen}.png'))
+            print(f'Best fitness: {pop.images[0].fitness}')
+
+
+def train_acc(accuracy, mutation_rate):
+    prev_best = pop.images[0]
+    gen = 0
+    acc = pop.images[0].fitness / (pop.images[0].image.shape[0] * pop.images[0].image.shape[1])
+    while acc < accuracy:
+        gen += 1
+        print(f'Training generation {gen}...')
+        pop.natural_selection(mutation_rate)
+        if False in (prev_best.image == pop.images[0].image):
+            acc = (pop.images[0].fitness / (pop.images[0].image.shape[0] * pop.images[0].image.shape[1]))
+            prev_best = pop.images[0]
+            pop.images[0].save_image(os.path.join('output_data', f'gen {gen}.png'))
+            print(f'Best fitness: {pop.images[0].fitness}')
+            print(f'Accuracy: {acc}')
 
 
 training_folder = os.path.join('training_data', 'triangle')
 training_paths = [os.path.join(training_folder, item) for item in os.listdir(training_folder)]
 training_images = [resize(io.imread(path, as_gray=True), (32, 32)) for path in training_paths]
-pop = Population(50, training_images[0].shape)
-train(1000)
+pop = Population(100, training_images[0].shape)
+# train_gens(10000, 1 / 1024)
+train_acc(0.75, 1 / 128)
