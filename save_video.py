9504922a030ff6383d15ed625de3bdae2376fe29
Philip Waldman <philip.waldman01@gmail.com>
2020-12-18 16:33:24 +0100

    Video can be saved to specified resolution.

--- save_video.py
+++ save_video.py
@@ -1,20 +1,29 @@
 import os
+from typing import Tuple
 
 import cv2
-from skimage import io
+from skimage import img_as_ubyte
+from skimage.io import imread
+from skimage.transform import resize
 
 
-def save_video(frame_dirs):
+def save_video(frame_dirs, resolution: Tuple[int, int] = None):
     print('\nSaving as video...')
     FPS = 24
 
-    # TODO: needs to be saved without compression
     fourcc = cv2.VideoWriter_fourcc(*'MP42')
-    video = cv2.VideoWriter(os.path.join('.', 'output_data', 'video', 'genetic.avi'),
-                            fourcc, float(FPS), io.imread(frame_dirs[0]).shape, isColor=False)
+    if resolution is None:
+        video = cv2.VideoWriter(os.path.join('.', 'output_data', 'video', 'genetic.avi'),
+                                fourcc, float(FPS), imread(frame_dirs[0]).shape, isColor=False)
+    else:
+        video = cv2.VideoWriter(os.path.join('.', 'output_data', 'video', 'genetic.avi'),
+                                fourcc, float(FPS), resolution, isColor=False)
 
     for im_path in frame_dirs:
-        im = io.imread(im_path)
+        if resolution is None:
+            im = imread(im_path)
+        else:
+            im = img_as_ubyte(resize(imread(im_path, as_gray=True), resolution, anti_aliasing=False, order=0))
         video.write(im)
     video.release()
     print('Video saved.')
